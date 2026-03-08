4763d376abc16419533934ed06255a15bfc60d39
Cesahz <espinolac999@gmail.com>
2026-03-08 15:50:32 -0300

    implementacion de algoritmo DFS para generacion automatica del mapa

--- Navegador_Algoritmico.py
+++ Navegador_Algoritmico.py
@@ -1,4 +1,6 @@
 import heapq
+from Motor_Logico import LIBRE, OBJETO, INICIO, FIN
+import random
 
 class BuscadorRutas:
     def __init__(self,motor_mapa):
@@ -63,4 +65,58 @@ class BuscadorRutas:
             actual = diccionario_padres[actual]
         camino.reverse()
         return camino
+    
+    def generar_laberinto_dfs(self):
+        #aislar el entorno, llenar todo de muros
+        self.motor.limpiar_todo_el_mapa()
+        for y in range(self.motor.alto_y):
+            for x in range(self.motor.ancho_x):
+                self.motor.matriz[y][x] = OBJETO
+
+        #vectores de direccion (salto de 2 en 2 )
+        #orden: arriba, derecha, abajo, izquierda
+        dx_gen = [0, 2, 0, -2]
+        dy_gen = [-2, 0, 2, 0]
+
+        #funcion recursiva interna
+        def excavar_recursivo(x, y):
+            #cavar el camino en la posicion actual
+            self.motor.matriz[y][x] = LIBRE
             
+            #preparar y mezclar direcciones
+            direcciones = [0, 1, 2, 3]
+            random.shuffle(direcciones)
+            
+            #explorar cada direccion
+            for i in range(4):
+                dir_idx = direcciones[i]
+                
+                #nuevas coordenadas con salto de 2
+                nx = x + dx_gen[dir_idx]
+                ny = y + dy_gen[dir_idx]
+                
+                #validar que las nuevas coordenadas esten dentro de los limites absolutos
+                if nx > 0 and nx < self.motor.ancho_x - 1 and ny > 0 and ny < self.motor.alto_y - 1:
+                    if self.motor.matriz[ny][nx] == OBJETO:
+                        #romper el muro intermedio
+                        muro_x = x + (dx_gen[dir_idx] // 2)
+                        muro_y = y + (dy_gen[dir_idx] // 2)
+                        self.motor.matriz[muro_y][muro_x] = LIBRE
+                        
+                        #recursividad: viajar a la nueva posicion
+                        excavar_recursivo(nx, ny)
+
+        #iniciar en 1,1
+        x_inicio, y_inicio = 1, 1
+        excavar_recursivo(x_inicio, y_inicio)
+        
+        #establecer el inicio
+        self.motor.modificar_celda(x_inicio, y_inicio, INICIO)
+
+        #calcular la esquina opuesta para el fin
+        x_fin = self.motor.ancho_x - 2 if self.motor.ancho_x % 2 != 0 else self.motor.ancho_x - 3
+        y_fin = self.motor.alto_y - 2 if self.motor.alto_y % 2 != 0 else self.motor.alto_y - 3
+        
+        #forzamos la salida en esa posicion
+        self.motor.matriz[y_fin][x_fin] = LIBRE
+        self.motor.modificar_celda(x_fin, y_fin, FIN)
